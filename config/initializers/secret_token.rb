431c037431ec90244bad4ab80b7ce5c71413e45f
René van den Berg <r.van.den.berg@ogd.nl>
2013-09-28 17:58:13 +0200

    Bare-bones Rails4 application. For real this time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xerxes::Application.config.secret_key_base = 'e3c25745d81896daa4a3ecd6305cfe5fdfc5987e7edc749c8c71d24b9a2f51dabb1ffc1b150f5b85ed8d0e3c17948d9f553436c1bfa33ef966fcc2f31d5c3511'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xerxes::Application.config.session_store :cookie_store, key: '_xerxes_session'

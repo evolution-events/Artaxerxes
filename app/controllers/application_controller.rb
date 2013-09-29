3bdc75329d64b43766a4098d970e86154e1d146a
Brenda Hellenthal <brendahellenthal@gmail.com>
2013-09-29 18:12:17 +0200

    Set current_user according to url parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+
+  def set_current_user
+   @current_user = User.find_by(username: params['current_user'])
+  end
 end
+

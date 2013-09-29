93a9dcba7afc2c38cf9c2ebdf24fbe87daf712ac
René van den Berg <r.van.den.berg@ogd.nl>
2013-09-29 17:48:26 +0200

    Rename User#to_s to name and define #to_s as self.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ActiveRecord::Base
   has_many :registrations
   has_many :registered_events, through: :registrations
 
-  def to_s
-    if preposition? then
+  def name
+    if preposition.present? then
       "#{first_name} #{preposition} #{last_name}"
     else
       "#{first_name} #{last_name}"
     end
+  end
+
+  def to_s
+    name
   end
 end

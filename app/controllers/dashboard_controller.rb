d788fa011131be517d0fb1db30834269bfe04022
Matthijs Kooijman <matthijs@stdin.nl>
2013-09-29 17:44:29 +0200

    Show future events in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
+  before_action :set_future_events, only: [:show]
+
   def show
   end
+
+  private
+    def set_future_events
+      @future_events = Event.where("start_date > ?", Time.now)
+    end
 end

6e854131c2be92ff89697c4a3adc8d5bf53aee49
Brenda Hellenthal <brendahellenthal@gmail.com>
2013-10-31 21:15:54 +0100

    Fix the test for the registration view. Don't expect input but select.

--- spec/views/registrations/edit.html.erb_spec.rb
+++ spec/views/registrations/edit.html.erb_spec.rb
@@ -14,8 +14,8 @@ describe "registrations/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", registration_path(@registration), "post" do
-      assert_select "input#registration_user[name=?]", "registration[user]"
-      assert_select "input#registration_event[name=?]", "registration[event]"
+      assert_select "select#registration_user[name=?]", "registration[user]"
+      assert_select "select#registration_event[name=?]", "registration[event]"
       assert_select "input#registration_status[name=?]", "registration[status]"
     end
   end

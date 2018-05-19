07b3226419277792000cd12604d87d04a56ba08d
Brenda <brendahellenthal@gmail.com>
2018-05-19 16:59:13 +0200

    Add method __str__ to ArtaUser
    When displaying an instance of ArtaUser we want to see the full name. (For now this is the default get_full_name method, we might override this method in the future)

--- apps/people/models/artauser.py
+++ apps/people/models/artauser.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # For reference to this model, see https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
 class ArtaUser(AbstractUser):
-    pass
+
+	def __str__(self):
+		return self.get_full_name()

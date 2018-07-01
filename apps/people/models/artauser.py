4dd0c7e4ad61fc68a2ff13b55cd029138c4412f9
Matthijs Kooijman <matthijs@stdin.nl>
2018-07-01 22:17:17 +0200

    models/ArtaUser: fix flake8 errors

--- apps/people/models/artauser.py
+++ apps/people/models/artauser.py
@@ -1,13 +1,15 @@
 import reversion
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+
 
 class ArtaUserManager(BaseUserManager):
     # Dummy user manager, in case we need more methods later
     pass
+
 
 # For reference to this model, see
 # https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
@@ -15,7 +17,7 @@ class ArtaUserManager(BaseUserManager):
 class ArtaUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField( _('email address'), unique=True)
+    email = models.EmailField(_('email address'), unique=True)
 
     is_staff = models.BooleanField(
         _('staff status'),
@@ -28,7 +30,7 @@ class ArtaUser(AbstractBaseUser, PermissionsMixin):
         default=True,
         help_text=_(
             'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
+            'Unselect this instead of deleting accounts.',
         ),
     )
 
@@ -41,9 +43,7 @@ class ArtaUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     def get_full_name(self):
-        """
-        Return the first_name plus the last_name, with a space in between.
-        """
+        """Return the first_name plus the last_name, with a space in between."""
         full_name = '%s %s' % (self.first_name, self.last_name)
         full_name = full_name.strip()
         if not full_name:

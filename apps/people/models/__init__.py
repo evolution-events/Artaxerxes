091ad464d5791864432bb1fc7a9ae8caeace0697
Brenda <brendahellenthal@gmail.com>
2018-08-24 23:14:33 +0200

    [people] Add first draft of MedicalDetails model
    This is nowhere near final but introduces a first draft for the MedicalDetails
    model with two fields that are important when setting up an event.
    See for considerations issue #42.
    
    Also registers the model in __init__.py

--- apps/people/models/__init__.py
+++ apps/people/models/__init__.py
@@ -1,5 +1,6 @@
 from .artauser import ArtaUser
 from .address import Address
 from .emergency_contact import EmergencyContact
+from .medical_details import MedicalDetails
 
-__all__ = ['ArtaUser', 'Address', 'EmergencyContact']
+__all__ = ['ArtaUser', 'Address', 'EmergencyContact', 'MedicalDetails']
---
+++ apps/people/models/medical_details.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext
+from django.utils.translation import ugettext_lazy as _
+
+
+# TODO: dit afmaken nadat we een knoop hebben doorgehakt over wat we precies willen vragen en opslaan. zie ook issue 42
+class MedicalDetails(models.Model):
+    """
+    Medical information linked to a single ArtaUser
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    food_allergies = models.TextField(
+        verbose_name=_('Food allergies'),
+        help_text=_("Please specify any allergies that our kitchen staff should take into account. Please also "
+                    "mention the severity. Leave blank when you have no allergies."),
+        blank=True,
+        null=True
+    )
+
+    event_risks = models.TextField(
+        verbose_name=_('Take into account for event'),
+        help_text=_("Please specify any medical or psychological conditions that we should take into account while "
+                    "preparing for the event. For example if you are allergic to smoke machines, can not handle "
+                    "flashes of light, are claustrophobic etc."),
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return gettext('Medical details of user %(user)s') % {'user': self.user}
+
+    class Meta:
+        verbose_name = _('medical details')
+        verbose_name_plural = _('medical details')

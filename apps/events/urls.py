7e90a3ae60d53f9706ba7580b1a9470bfb816735
Brenda <brendahellenthal@gmail.com>
2018-08-25 00:52:35 +0200

    [events] Add list of events to register for
    Add view, template, url and menu item for showing a list of events one can
    register for (WIP, now shows just all events). (No register links yet, since
    they would not go anywhere yet)

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     url(r'^$', views.event_index_view, name='index'),
+    url(r'/list/', views.event_list_view, name='eventlist'),
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -11,3 +11,14 @@ def event_index_view(request):
         (TODO: really needed?)
         """
         return render(request, 'events/index.html', {'user': request.user})
+
+
+@login_required
+def event_list_view(request):
+    """
+    List of all events that people can register for.
+    """
+    # TODO: actually filter or select on events one can register for
+    events = Event.objects.all()
+
+    return render(request, 'events/list.html', {'user': request.user, 'events': events})

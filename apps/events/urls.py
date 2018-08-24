5627804c2401bc605b63dc1ea3129d15c1b6e511
Brenda <brendahellenthal@gmail.com>
2018-08-25 01:42:58 +0200

    [events:WIP] Add registration flow for frontend
    Add registration links (todo: show only one of them depending on event state),
    urls, view and templates to walk through the whole registration process in the
    frontend.
    Still a work in progress, lots of TODO's in the code and some placeholder code
    (like bogus field on event options form).

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,4 +6,8 @@ app_name = 'events'
 urlpatterns = [
     url(r'^$', views.event_index_view, name='index'),
     url(r'/list/', views.event_list_view, name='eventlist'),
+    url(r'r/pd/(?P<eventid>[0-9]+)/', views.registration_step_personal_details, name="personaldetailform"),
+    url(r'r/md/(?P<eventid>[0-9]+)/', views.registration_step_medical_details, name="medicaldetailform"),
+    url(r'r/op/(?P<eventid>[0-9]+)/', views.registration_step_options, name="optionsform"),
+    url(r'r/fc/(?P<eventid>[0-9]+)/', views.registration_step_final_check, name="finalcheckform"),
 ]

67f845fa2ae1386a8ba565d92b879a75782701bd
Matthijs Kooijman <matthijs@stdin.nl>
2019-11-09 14:34:13 +0100

    registrations: Create a registration before starting the process
    Additionally, pass the registration id along in the url, rather than the
    event id after the first step.

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'registrations'
 urlpatterns = [
-    url(r'r/pd/(?P<eventid>[0-9]+)/', views.registration_step_personal_details, name="personaldetailform"),
-    url(r'r/md/(?P<eventid>[0-9]+)/', views.registration_step_medical_details, name="medicaldetailform"),
-    url(r'r/op/(?P<eventid>[0-9]+)/', views.registration_step_options, name="optionsform"),
-    url(r'r/fc/(?P<eventid>[0-9]+)/', views.registration_step_final_check, name="finalcheckform"),
+    url(r'r/(?P<eventid>[0-9]+)/', views.RegistrationStartView.as_view(), name="prepare_registration"),
+    url(r'r/pd/(?P<registrationid>[0-9]+)/', views.registration_step_personal_details, name="personaldetailform"),
+    url(r'r/md/(?P<registrationid>[0-9]+)/', views.registration_step_medical_details, name="medicaldetailform"),
+    url(r'r/op/(?P<registrationid>[0-9]+)/', views.registration_step_options, name="optionsform"),
+    url(r'r/fc/(?P<registrationid>[0-9]+)/', views.registration_step_final_check, name="finalcheckform"),
 ]
--- apps/registrations/views.py
+++ apps/registrations/views.py
@@ -2,8 +2,10 @@
 import reversion
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext as _
+from django.views.generic.base import View
 
 from apps.events.models import Event
 from apps.people.models import Address, MedicalDetails
@@ -12,11 +14,22 @@ from .forms import FinalCheckForm, MedicalDetailForm, PersonalDetailForm, Regist
 from .models import Registration
 
 
+class RegistrationStartView(LoginRequiredMixin, View):
+    # TODO: This should be a POST request
+    def get(self, request, eventid):
+        event = get_object_or_404(Event, pk=eventid)
+        registration, created = Registration.objects.get_or_create(
+            event=event, user=request.user, defaults={'status': Registration.STATUS_PREPARATION_IN_PROGRESS},
+        )
+
+        return redirect('registrations:personaldetailform', registrationid=registration.id)
+
+
 @login_required
-def registration_step_personal_details(request, eventid=None):
+def registration_step_personal_details(request, registrationid=None):
     """ Step in registration process where user fills in personal details """
 
-    event = get_object_or_404(Event, pk=eventid)
+    registration = get_object_or_404(Registration, pk=registrationid)
     address = None
     if hasattr(request.user, 'address'):
         address = request.user.address
@@ -34,28 +47,25 @@ def registration_step_personal_details(request, eventid=None):
                              extra_tags='bg-success')  # add bootstrap css class
             # TODO: send verification e-mail for e-address after figuring out what to use
             # Make registration and set status to 'in STATUS_PREPARATION_IN_PROGRESS'
-            registration = Registration.objects.create(user=request.user, event=event,
-                                                       status=Registration.STATUS_PREPARATION_IN_PROGRESS)
-            registration.save()
-            return redirect('registrations:medicaldetailform', eventid=event.id)
+            return redirect('registrations:medicaldetailform', registrationid=registration.id)
         else:
             messages.error(request, _('Please correct the error below.'), extra_tags='bg-danger')
     else:
         pd_form = PersonalDetailForm(instance=address)
     return render(request, 'registrations/editpersonaldetails.html', {
         'pd_form': pd_form,
-        'event': event,
+        'registration': registration,
     })
 
 
 @login_required
-def registration_step_medical_details(request, eventid=None):
+def registration_step_medical_details(request, registrationid=None):
     """ Step in registration process where user fills in medical details """
 
     mdetails = None
     if hasattr(request.user, 'medicaldetails'):
         mdetails = request.user.medicaldetails
-    event = get_object_or_404(Event, pk=eventid)
+    registration = get_object_or_404(Registration, pk=registrationid)
     if request.method == 'POST':
         md_form = MedicalDetailForm(request.POST, instance=mdetails)
         if md_form.is_valid():
@@ -68,24 +78,25 @@ def registration_step_medical_details(request, eventid=None):
                                       "fields changed: %(fields)s" % {'fields': ", ".join(md_form.changed_data)}))
             messages.success(request, _('Your medical information was successfully updated!'),
                              extra_tags='bg-success')  # add bootstrap css class
-            return redirect('registrations:optionsform', eventid=event.id)
+            return redirect('registrations:optionsform', registrationid=registration.id)
         else:
             messages.error(request, _('Please correct the error below.'), extra_tags='bg-danger')
     else:
         md_form = MedicalDetailForm(instance=mdetails)
     return render(request, 'registrations/editmedicaldetails.html', {
         'md_form': md_form,
-        'event': event,
+        'registration': registration,
     })
 
 
 @login_required
-def registration_step_options(request, eventid=None):
+def registration_step_options(request, registrationid=None):
     """ Step in registration process where user chooses options """
 
-    event = get_object_or_404(Event, pk=eventid)
+    registration = get_object_or_404(Registration, pk=registrationid)
+    # TODO: Check registration status?
     if request.method == 'POST':
-        opt_form = RegistrationOptionsForm(event, request.user, data=request.POST)
+        opt_form = RegistrationOptionsForm(registration.event, request.user, data=request.POST)
         if opt_form.is_valid():
             # TODO save form and link event?, user?
             """with reversion.create_revision():
@@ -96,33 +107,32 @@ def registration_step_options(request, eventid=None):
             messages.success(request, _('Your options were successfully registered!'),
                              extra_tags='bg-success')  # add bootstrap css class
             """
-            return redirect('registrations:finalcheckform', eventid=event.id)
+            return redirect('registrations:finalcheckform', registrationid=registration.id)
         else:
             messages.error(request, _('Please correct the error below.'), extra_tags='bg-danger')
     else:
-        opt_form = RegistrationOptionsForm(event, request.user)
+        opt_form = RegistrationOptionsForm(registration.event, request.user)
     return render(request, 'registrations/editoptions.html', {
         'opt_form': opt_form,
-        'event': event,
+        'registration': registration,
     })
 
 
 @login_required
-def registration_step_final_check(request, eventid=None):
+def registration_step_final_check(request, registrationid=None):
     """ Step in registration process where user checks all information and agrees to conditions """
 
-    event = get_object_or_404(Event, pk=eventid)
+    registration = get_object_or_404(Registration, pk=registrationid)
     personal_details = Address.objects.filter(user=request.user).first()  # Returns None if nothing was found
     medical_details = MedicalDetails.objects.filter(user=request.user).first()  # Returns None if nothing was found
     # TODO: get chosen event options and pass them on when rendering the page
+    # TODO: Check registration status?
 
     if request.method == 'POST':
         fc_form = FinalCheckForm(request.POST)
         if fc_form.is_valid():
 
             with reversion.create_revision():
-                cancelled = Registration.STATUS_CANCELLED
-                registration = Registration.objects.filter(user=request.user, event=event).exclude(status=cancelled)[0]
                 registration.status = Registration.STATUS_PREPARATION_COMPLETE
                 registration.save()
                 reversion.set_user(request.user)
@@ -137,7 +147,7 @@ def registration_step_final_check(request, eventid=None):
         fc_form = FinalCheckForm()
     return render(request, 'registrations/finalcheck.html', {
         'user': request.user,
-        'event': event,
+        'registration': registration,
         'pdetails': personal_details,
         'mdetails': medical_details,
         'fc_form': fc_form,

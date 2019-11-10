b19565635589df20912004b03963e91ca0bd8357
Matthijs Kooijman <matthijs@stdin.nl>
2019-11-10 17:40:04 +0100

    registrations: Improve (prepare) registration flow
    This makes all kinds of changes around the registration flow, which is
    now fairly complete except for the actual registration finalization.

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'registrations'
 urlpatterns = [
-    path('<int:eventid>/', views.RegistrationStartView.as_view(), name="prepare_registration"),
+    path('<int:eventid>/', views.RegistrationStartView.as_view(), name="register"),
     path('pd/<int:registrationid>/', views.registration_step_personal_details, name="personaldetailform"),
     path('md/<int:registrationid>/', views.registration_step_medical_details, name="medicaldetailform"),
     path('op/<int:registrationid>/', views.registration_step_options, name="optionsform"),
--- apps/registrations/views.py
+++ apps/registrations/views.py
@@ -21,8 +21,10 @@ class RegistrationStartView(LoginRequiredMixin, View):
         registration, created = Registration.objects.get_or_create(
             event=event, user=request.user, defaults={'status': Registration.STATUS_PREPARATION_IN_PROGRESS},
         )
-
-        return redirect('registrations:personaldetailform', registrationid=registration.id)
+        if registration.status == Registration.STATUS_PREPARATION_IN_PROGRESS:
+            return redirect('registrations:personaldetailform', registrationid=registration.id)
+        else:
+            return redirect('registrations:finalcheckform', registrationid=registration.id)
 
 
 @login_required
@@ -30,6 +32,8 @@ def registration_step_personal_details(request, registrationid=None):
     """ Step in registration process where user fills in personal details """
 
     registration = get_object_or_404(Registration, pk=registrationid)
+    # Get a copy of the event annotated for this user
+    event = Event.objects.for_user(request.user).get(pk=registration.event.pk)
     address = None
     if hasattr(request.user, 'address'):
         address = request.user.address
@@ -43,8 +47,7 @@ def registration_step_personal_details(request, registrationid=None):
                 reversion.set_user(request.user)
                 reversion.set_comment(_("Personal info updated via frontend. The following "
                                       "fields changed: %(fields)s" % {'fields': ", ".join(pd_form.changed_data)}))
-            messages.success(request, _('Your personal information was successfully updated!'),
-                             extra_tags='bg-success')  # add bootstrap css class
+
             # TODO: send verification e-mail for e-address after figuring out what to use
             # Make registration and set status to 'in STATUS_PREPARATION_IN_PROGRESS'
             return redirect('registrations:medicaldetailform', registrationid=registration.id)
@@ -55,6 +58,7 @@ def registration_step_personal_details(request, registrationid=None):
     return render(request, 'registrations/editpersonaldetails.html', {
         'pd_form': pd_form,
         'registration': registration,
+        'event': event,
     })
 
 
@@ -66,6 +70,8 @@ def registration_step_medical_details(request, registrationid=None):
     if hasattr(request.user, 'medicaldetails'):
         mdetails = request.user.medicaldetails
     registration = get_object_or_404(Registration, pk=registrationid)
+    # Get a copy of the event annotated for this user
+    event = Event.objects.for_user(request.user).get(pk=registration.event.pk)
     if request.method == 'POST':
         md_form = MedicalDetailForm(request.POST, instance=mdetails)
         if md_form.is_valid():
@@ -76,8 +82,7 @@ def registration_step_medical_details(request, registrationid=None):
                 reversion.set_user(request.user)
                 reversion.set_comment(_("Medical info updated via frontend. The following "
                                       "fields changed: %(fields)s" % {'fields': ", ".join(md_form.changed_data)}))
-            messages.success(request, _('Your medical information was successfully updated!'),
-                             extra_tags='bg-success')  # add bootstrap css class
+
             return redirect('registrations:optionsform', registrationid=registration.id)
         else:
             messages.error(request, _('Please correct the error below.'), extra_tags='bg-danger')
@@ -86,6 +91,7 @@ def registration_step_medical_details(request, registrationid=None):
     return render(request, 'registrations/editmedicaldetails.html', {
         'md_form': md_form,
         'registration': registration,
+        'event': event,
     })
 
 
@@ -94,6 +100,12 @@ def registration_step_options(request, registrationid=None):
     """ Step in registration process where user chooses options """
 
     registration = get_object_or_404(Registration, pk=registrationid)
+    # Get a copy of the event annotated for this user
+    event = Event.objects.for_user(request.user).get(pk=registration.event.pk)
+
+    if not event.registration_fields.all():
+        return redirect('registrations:finalcheckform', registrationid=registration.id)
+
     # TODO: Check registration status?
     if request.method == 'POST':
         opt_form = RegistrationOptionsForm(registration.event, request.user, data=request.POST)
@@ -103,8 +115,13 @@ def registration_step_options(request, registrationid=None):
                 reversion.set_user(request.user)
                 reversion.set_comment(_("Options updated via frontend. The following "
                                       "fields changed: %(fields)s" % {'fields': ", ".join(opt_form.changed_data)}))
-            messages.success(request, _('Your options were successfully registered!'),
-                             extra_tags='bg-success')  # add bootstrap css class
+
+            if registration.status == Registration.STATUS_PREPARATION_IN_PROGRESS:
+                with reversion.create_revision():
+                    registration.status = Registration.STATUS_PREPARATION_COMPLETE
+                    registration.save()
+                    reversion.set_user(request.user)
+                    reversion.set_comment(_("Registration preparation finalized via frontend."))
 
             return redirect('registrations:finalcheckform', registrationid=registration.id)
         else:
@@ -114,6 +131,7 @@ def registration_step_options(request, registrationid=None):
     return render(request, 'registrations/editoptions.html', {
         'opt_form': opt_form,
         'registration': registration,
+        'event': event,
     })
 
 
@@ -122,6 +140,8 @@ def registration_step_final_check(request, registrationid=None):
     """ Step in registration process where user checks all information and agrees to conditions """
 
     registration = get_object_or_404(Registration, pk=registrationid)
+    # Get a copy of the event annotated for this user
+    event = Event.objects.for_user(request.user).get(pk=registration.event.pk)
     personal_details = Address.objects.filter(user=request.user).first()  # Returns None if nothing was found
     medical_details = MedicalDetails.objects.filter(user=request.user).first()  # Returns None if nothing was found
     # TODO: Check registration status?
@@ -129,15 +149,7 @@ def registration_step_final_check(request, registrationid=None):
     if request.method == 'POST':
         fc_form = FinalCheckForm(request.POST)
         if fc_form.is_valid():
-
-            with reversion.create_revision():
-                registration.status = Registration.STATUS_PREPARATION_COMPLETE
-                registration.save()
-                reversion.set_user(request.user)
-                reversion.set_comment(_("Registration preparation finalized via frontend."))
-            messages.success(request, _('You have completed your preparation for registration!'),
-                             extra_tags='bg-success')  # add bootstrap css class
-
+            # TODO Finalize registration
             return redirect('events:eventlist')
         else:
             messages.error(request, _('Please correct the error below.'), extra_tags='bg-danger')
@@ -146,6 +158,7 @@ def registration_step_final_check(request, registrationid=None):
     return render(request, 'registrations/finalcheck.html', {
         'user': request.user,
         'registration': registration,
+        'event': event,
         'pdetails': personal_details,
         'mdetails': medical_details,
         'fc_form': fc_form,

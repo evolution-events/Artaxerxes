7532c450b5b8bcd86a8136375706bd94bba8c98d
Rene van den Berg <rene@ranj.nl>
2020-01-19 14:18:11 +0100

    Remove slash from url pattern for organisation info to prevent warnings

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', views.main_index_view, name='main_index_view'),
-    path('/organisation_info', views.organisation_info_view, name='organisation_info_view'),
+    path('organisation_info', views.organisation_info_view, name='organisation_info_view'),
 ]

95016baa6490da1419e8abf4126e8637e7f29f36
Brenda <brendahellenthal@gmail.com>
2020-01-21 16:53:59 +0100

    Use class based view for dashboard page
    Turn function view into class based view and update the line in urls.py
    accordingly.
    Also collect and pass on some info already in the new view.

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,12 +1,34 @@
-# from django.shortcuts import render
+import collections
+from datetime import date
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import View
+
+from apps.events.models import Event
 
 
-@login_required
-def main_index_view(request):
-    """ Present a dashboard page to the user. """
-    return render(request, 'core/dashboard.html')
+class RegistrationsDashboardView(LoginRequiredMixin, View):
+    def get(self, request):
+        events = Event.objects.for_user(request.user, with_registration_status=True).filter(is_visible=True)
+
+        def group(e):
+            if e.start_date > date.today():
+                if e.registration_status and e.registration_status.ACTIVE:
+                    return 'active'
+                else:
+                    return 'future'
+
+        grouped = collections.defaultdict(list)
+        for e in events:
+            grouped[group(e)].append(e)
+
+        context = {
+            'user': request.user,
+            'events': grouped,
+        }
+        return render(request, 'core/dashboard.html', context)
 
 
 @login_required

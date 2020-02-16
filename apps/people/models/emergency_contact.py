cae50a2f46885f0f8be18533aea07502cfdbb06e
Matthijs Kooijman <matthijs@stdin.nl>
2020-02-16 17:33:51 +0100

    EmergencyContact: Make relation field optional

--- apps/people/models/emergency_contact.py
+++ apps/people/models/emergency_contact.py
@@ -15,7 +15,7 @@ class EmergencyContact(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='emergency_contacts')
     contact_name = models.CharField(max_length=100, verbose_name=_('Name of contact'))
-    relation = models.CharField(max_length=100, verbose_name=_('Relation to contact'),
+    relation = models.CharField(max_length=100, verbose_name=_('Relation to contact'), blank=True,
                                 help_text=_('For example: parent, partner, friend, etc.'))
     phone_number = PhoneNumberField(verbose_name=_('Phone number of contact'))
     remarks = models.CharField(max_length=200, verbose_name=_('Remarks'), blank=True)

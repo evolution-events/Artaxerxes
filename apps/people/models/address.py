95d689688404298872525ccb9e7aff6545154a45
Matthijs Kooijman <matthijs@stdin.nl>
2020-02-16 17:30:23 +0100

    Address: Add phone_number field

--- apps/people/models/address.py
+++ apps/people/models/address.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext
 from django.utils.translation import ugettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 @reversion.register()
@@ -10,6 +11,7 @@ class Address(models.Model):
     """Address information linked to a single ArtaUser."""
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(verbose_name=_('Phone number'))
     address = models.CharField(max_length=100, verbose_name=_('Address'))
     postalcode = models.CharField(max_length=10, verbose_name=_('Postal code'))
     city = models.CharField(max_length=100, verbose_name=_('City of residence'))

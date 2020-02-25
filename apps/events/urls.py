3b56dd838f05216c52c2dd2a02daf014ac1fb723
Rene van den Berg <rene@ranj.nl>
2020-02-25 01:08:27 +0100

    Fix: remove the event landing page since it does not exist in the first place and seems not to be needed right now.

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'events'
 urlpatterns = [
-    path('', views.event_index_view, name='index'),
     path('list/', views.event_list_view, name='eventlist'),
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -8,15 +8,6 @@ from .models import Event
 
 
 @login_required
-def event_index_view(request):
-    """ Landing page for events.
-
-    (TODO: really needed?)
-    """
-    return render(request, 'events/index.html', {'user': request.user})
-
-
-@login_required
 def event_list_view(request):
     """ List of all events that people can register or have registered for. """
     events = Event.objects.for_user(request.user, with_registration_status=True).filter(is_visible=True)

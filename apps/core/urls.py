e7c69dbeaab99ce6eece375f4ab94f6bd11ddfff
Matthijs Kooijman <matthijs@stdin.nl>
2020-03-08 21:33:05 +0100

    urls: Add missing comma
    This fixes a flake8 warning.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,5 +6,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.RegistrationsDashboardView.as_view(), name='main_index_view'),
     path('practical_info', views.practical_info_view, name='practical_info_view'),
-    path('about_this_system', views.AboutArtaView.as_view(), name='about_arta_view')
+    path('about_this_system', views.AboutArtaView.as_view(), name='about_arta_view'),
 ]

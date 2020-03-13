98d133869d238fba089f94db43a6c581e56193f4
Matthijs Kooijman <matthijs@stdin.nl>
2020-03-13 11:19:16 +0100

    settings: Prepare production config for deployment

--- arta/settings/production.py
+++ arta/settings/production.py
@@ -1,5 +1,5 @@
 # for now fetch the development settings only
-from .development import *
+from .common import *
 
 # turn off all debugging
 DEBUG = False
@@ -7,27 +7,74 @@ DEBUG = False
 # You will have to determine, which hostnames should be served by Django
 ALLOWED_HOSTS = []
 
+# This logs to stdout/stderr, which ends up in the default uwsgi log
+# files.
+LOGGING = {
+    'version': 1,
+    # Recommended, otherwise default loggers are disabled but not removed, apparently?
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} - {levelname} - {message}',
+            'style': '{',
+            # This mimics the (rather interesting) uwsgi data format to
+            # get a unified log output
+            'datefmt': '%a %b %d %H:%M:%S %Y',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
+
+# ##### SERVER CONFIGURATION ##############################
+ALLOWED_HOSTS = ['arta.evolution-events.nl', 'artaxerxes.evolution-events.nl', 'registrations.evolution-events.nl']
+
+# ##### DATABASE CONFIGURATION ############################
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'db.local',
+        'USER': 'ee_artaxerxes',
+        # From local_settings
+        'PASSWORD': MYSQL_PASSWORD,
+        'NAME': 'ee_artaxerxes',
+    },
+}
+
+# ##### EMAIL CONFIGURATION ###############################
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.local'
+
 # ##### SECURITY CONFIGURATION ############################
 
-# TODO: Make sure, that sensitive information uses https
-# TODO: Evaluate the following settings, before uncommenting them
-# redirects all requests to https
-# SECURE_SSL_REDIRECT = True
-# session cookies will only be set, if https is used
-# SESSION_COOKIE_SECURE = True
-# how long is a session cookie valid?
-# SESSION_COOKIE_AGE = 1209600
+# Note: Webserver guarantees only secure requests are processed and the
+# (u)wsgi-protocol seems to pass on secure status automatically. The
+# webserver also sets HSTS headers.
+# Even so, let Django redirect to HTTPS as well, just in case the
+# webserver config gets messed up.
+SECURE_SSL_REDIRECT = True
+# Session cookies will be marked as secure, so the browser will only
+# send them over HTTPS
+SESSION_COOKIE_SECURE = True
 
 # validates passwords (very low security, but hey...)
-# AUTH_PASSWORD_VALIDATORS = [
-#    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-#    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-#    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-#    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+]
 
-# the email address, these error notifications to admins come from
-# SERVER_EMAIL = 'root@localhost'
-
-# how many days a password reset should work. I'd say even one day is too long
-# PASSWORD_RESET_TIMEOUT_DAYS = 1
+# How long a password reset link should work.
+# TODO: A day is probably too long already, in Django (the upcoming) 3.1 this can be
+# specified in seconds instead.
+PASSWORD_RESET_TIMEOUT_DAYS = 1

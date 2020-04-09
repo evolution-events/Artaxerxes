322c881b76cf38a6f75205fcf2d9ab0d96df0045
Matthijs Kooijman <matthijs@stdin.nl>
2020-04-09 12:17:35 +0200

    MedicalDetailsFactory: Add class

--- apps/people/tests/factories.py
+++ apps/people/tests/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from ..models import ArtaUser
+from ..models import ArtaUser, MedicalDetails
 
 
 class ArtaUserFactory(factory.django.DjangoModelFactory):
@@ -9,3 +9,11 @@ class ArtaUserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     email = factory.Faker('email')
+
+
+class MedicalDetailsFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MedicalDetails
+    food_allergies = factory.Faker('sentence')
+    event_risks = factory.Faker('text')
+    user = factory.SubFactory(ArtaUserFactory)

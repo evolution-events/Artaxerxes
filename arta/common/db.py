d38941d818cda2d9eeff4ee6be92e1f135426a91
Matthijs Kooijman <matthijs@stdin.nl>
2020-06-18 04:40:33 +0200

    FromOuterRef: Add helper class

--- arta/common/db.py
+++ arta/common/db.py
@@ -7,6 +7,21 @@ def QExpr(*args, **kwargs):
     return models.ExpressionWrapper(models.Q(*args, **kwargs), output_field=models.BooleanField())
 
 
+class FromOuterRef:
+    """
+    Helper to allow using the same query for a direct or a subquery.
+
+    A function that generates a query based on a model instance, and only uses direct fields of that model instance,
+    can be passed an instance of FromOuterRef instead to generate a query with OuterRef fields instead.
+    """
+
+    def __init__(self, prefix=''):
+        self.prefix = prefix
+
+    def __getattr__(self, attr):
+        return models.OuterRef(self.prefix + attr)
+
+
 class UpdatedAtQuerySetMixin:
     def update(self, **kwargs):
         # TODO: This should be implemented if we need QuerySet.update, but for now just raise

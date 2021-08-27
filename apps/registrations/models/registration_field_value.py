169a10fdf158725d58068649cc81fee9059e610e
Matthijs Kooijman <matthijs@stdin.nl>
2021-08-27 19:13:58 +0200

    registrations: Show registration options grouped by section
    This affects various places where registration options are displayed
    (dashboard, registration confirmation page and email, final check page).
    
    The table-based HTML version is still a bit rough and needs still some
    restyling.

--- apps/registrations/models/registration_field_value.py
+++ apps/registrations/models/registration_field_value.py
@@ -13,6 +13,37 @@ class RegistrationFieldValueQuerySet(UpdatedAtQuerySetMixin, models.QuerySet):
         """ Wrapper for select_related, to be used from templates """
         return self.select_related('option', 'field')
 
+    def group_by_section(self):
+        """
+        Group the results of this query by the section of related field.
+
+        This resolves this queryset and returns a list of (section, values) tuples, where section is a
+        RegistrationField option, and values is a list of RegistrationFieldValue objects. The resulting values are
+        ordered based on the field ordering, but only values in this queryset are returned (but empty sections are
+        omitted).
+        """
+        our_options = {value.field_id: value for value in self}
+        if not our_options:
+            return
+        any_option = next(iter(our_options.values()))
+        event_id = any_option.field.event_id
+
+        all_fields = RegistrationField.objects.all().filter(event=event_id)
+        section = None
+        fields = []
+
+        for field in all_fields:
+            if field.field_type.SECTION:
+                if fields:
+                    yield (section, fields)
+                section = field
+                fields = []
+            elif field.id in our_options:
+                fields.append(our_options[field.pk])
+
+        if fields:
+            yield (section, fields)
+
 
 class RegistrationFieldValueManager(models.Manager.from_queryset(RegistrationFieldValueQuerySet)):
     pass

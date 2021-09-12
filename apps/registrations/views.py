evolution-events/Artaxerxes

commit 833d5284c145bc0c8ce4197e3407de403b4fc3b8

RegistrationStart: Make subclass of RegistrationStepMixin
This also includes the start step in REGISTRATION_STEPS and lets it use
step_base.html.

There is a small change in behavior when doing a POST to the start view
when the registration is already in PREPARATION_COMPLETED, which now
redirects to finalcheck immediately rather than to the first options
step. This affects one testcase, which is updated accordingly.

apps/registrations/views.py
@@ -10,8 +10,8 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
-from django.views.generic import DetailView, View
-from django.views.generic.base import ContextMixin, TemplateResponseMixin
+from django.views.generic import DetailView
+from django.views.generic.base import ContextMixin, TemplateView
 from django.views.generic.edit import FormView
 
 from apps.events.models import Event
@@ -25,6 +25,9 @@ from .services import RegistrationNotifyService, RegistrationStatusService
 
 REGISTRATION_STEPS = [
     {
+        'view': 'registrations:registration_start',
+        'statuses': [],
+    }, {
         'view': 'registrations:step_registration_options',
         'cancel_view': 'core:dashboard',
         'statuses': Registration.statuses.DRAFT,
@@ -134,34 +137,37 @@ class RegistrationStepMixin(LoginRequiredMixin, CacheUsingTimestampsMixin, Regis
     pass
 
 
-class RegistrationStart(LoginRequiredMixin, TemplateResponseMixin, View):
+class RegistrationStart(RegistrationStepMixin, TemplateView):
     template_name = 'registrations/registration_start.html'
 
-    def get(self, request, eventid):
-        event = get_object_or_404(Event.objects.for_user(request.user), pk=eventid)
+    @cached_property
+    def registration(self):
         try:
-            registration = Registration.objects.get(
-                event=event,
-                user=request.user,
+            return self.get_queryset().get(
+                event=self.event,
+                user=self.request.user,
                 is_current=True,
             )
-            if registration.status.PREPARATION_IN_PROGRESS:
-                return redirect('registrations:step_registration_options', registration.id)
-            else:
-                return redirect('registrations:step_final_check', registration.id)
         except Registration.DoesNotExist:
-            return self.render_to_response({
-                'event': event,
-            })
+            return None
+
+    @cached_property
+    def event(self):
+        return get_object_or_404(Event.objects.for_user(self.request.user), pk=self.kwargs['eventid'])
+
+    def check_request(self):
+        if self.registration:
+            # If a registration already exists, let our parent figure out where to redirect
+            return super().check_request()
+        return None
 
     def post(self, request, eventid):
-        event = get_object_or_404(Event.objects.for_user(request.user), pk=eventid)
         with reversion.create_revision():
             reversion.set_user(self.request.user)
             reversion.set_comment(_("Registration started via frontend."))
 
             registration, created = Registration.objects.filter(is_current=True).get_or_create(
-                event=event,
+                event=self.event,
                 user=request.user,
                 defaults={'status': Registration.statuses.PREPARATION_IN_PROGRESS},
             )

apps/registrations/templates/registrations/registration_start.html
@@ -1,4 +1,4 @@
-{% extends "base.html" %}
+{% extends "registrations/step_base.html" %}
 {% load i18n %}
 
 {% block pagetitle %}
@@ -9,7 +9,7 @@
   {% endif %}
 {% endblock pagetitle%}
 
-{% block content %}
+{% block beforeform %}
   {% if not event.registration_is_open %}
   <p>{% blocktrans %}
 	Registration for {{event}} is not yet open. However, you can already
@@ -27,13 +27,12 @@
   you will finalize your registration and actually be registered for the
   event.
   {% endblocktrans %}</p>
+{% endblock beforeform %}
 
-  <form method="post" enctype="multipart/form-data">
-    {% csrf_token %}
+{% block buttons %}
     {% if not event.registration_is_open %}
     <button class="btn btn-success" type="submit">{% trans 'Start registration' %}</button>
     {% else %}
     <button class="btn btn-success" type="submit">{% trans 'Prepare registration' %}</button>
     {% endif %}
-  </form>
-{% endblock content %}
+{% endblock buttons %}

apps/registrations/tests/test_registration.py
@@ -587,10 +587,10 @@ class TestRegistrationForm(TestCase, AssertHTMLMixin):
         self.assertEqual(Registration.objects.all().count(), 1)
         self.assertEqual(reg.status, Registration.statuses.PREPARATION_COMPLETE)
 
-        # And posting again should still redirect to the first step, without creating a new Registration or modifying
-        # the status.
+        # And posting again should now redirect to final_check, without creating a new Registration or modifying the
+        # status.
         response = self.client.post(start_url)
-        self.assertRedirects(response, first_step_url)
+        self.assertRedirects(response, final_check_url)
         self.assertEqual(Registration.objects.all().count(), 1)
         self.assertEqual(reg.status, Registration.statuses.PREPARATION_COMPLETE)
 

commit 4f8990140c1de249316d79ad03597a92b648b85f

FinalCheck: Fix "Modify information" button
This accidentally pointed to the wrong url. This was probably broken in
commit 833d528 (RegistrationStart: Make subclass of RegistrationStepMixin).

apps/registrations/views.py
@@ -334,7 +334,9 @@ class FinalCheck(RegistrationStepMixin, FormView):
     form_class = FinalCheckForm
 
     def get_modify_url(self):
-        return reverse(REGISTRATION_STEPS[0]['view'], args=(self.registration.pk,))
+        # TODO: This hardcodes skipping the first "start" step, but
+        # maybe we can do this more elegantly?
+        return reverse(REGISTRATION_STEPS[1]['view'], args=(self.registration.pk,))
 
     def form_valid(self, form):
         try:
6bb58b6467ea1c2efffd3221b1fe0130a52548ac
Matthijs Kooijman <matthijs@stdin.nl>
2022-02-11 21:40:49 +0100

    RegisteredEventList: Convert to use ListView
    This makes it slightly more declaratative (and makes it easier to add
    pagination in the future).

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,28 +1,30 @@
 from datetime import date
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from apps.registrations.models import Registration
 
 from .models import Event
 
 
-class RegisteredEventList(LoginRequiredMixin, TemplateView):
-    """ List of all events that the current user is registred for. """
+class RegisteredEventList(LoginRequiredMixin, ListView):
+    """ List of all events that the current user is registered for. """
 
     template_name = 'events/registered_events.html'
+    model = Event
+    ordering = '-start_date'
 
-    def get_context_data(self, **kwargs):
-        registered_events = (
-            Event.objects
+    def get_queryset(self):
+        return (
+            super().get_queryset()
             .for_user(self.request.user, with_registration=True)
             .filter(registration_status__in=Registration.statuses.FINALIZED)
-            .order_by('-start_date')
         )
 
+    def get_context_data(self, **kwargs):
         events = {'future': [], 'past': []}
-        for e in registered_events:
+        for e in self.object_list:
             if e.start_date > date.today():
                 events['future'].append(e)
             else:
